8edfaf4a7430746cb564e83d3ba28eb4025f507f
Didar Daniyarbekov <Didar.Daniyarbekov@gmail.com>
2022-09-27 17:23:25 +0600

    Добавил вью добавления product
    создан шаблон страницы
    добавил путь страницы

--- market/webapp/urls.py
+++ market/webapp/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path
 from webapp.views.index import index_view
 from webapp.views.category import category_add_view
+from webapp.views.product import product_add_view
 
 
 
 urlpatterns= [
     path("", index_view, name='index'),
     path("products", index_view, name='products'),
-    path("categories/add", category_add_view, name='category_add')
+    path("categories/add", category_add_view, name='category_add'),
+    path("products/add", product_add_view, name='product_add')
 ]
---
+++ market/webapp/views/product.py
@@ -0,0 +1,22 @@
+from django.urls import reverse
+from django.shortcuts import render, redirect, get_object_or_404
+from webapp.models import Product, Category
+
+
+def product_add_view(request):
+    if request.method == 'GET':
+        context = {
+            'categories' : Category.objects.all()
+        }
+        print(context)
+        return render(request, 'add_product.html', context)
+    product_data = {
+        'name': request.POST.get('name'),
+        'description': request.POST.get('description'),
+        'price': request.POST.get('price'),
+        'image': request.POST.get('image'),
+        'category': Category.objects.get(name=request.POST.get('category'))
+    }
+    product = Product.objects.create(**product_data)
+    return redirect("/")
+
