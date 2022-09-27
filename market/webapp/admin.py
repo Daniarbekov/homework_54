4b107d16b1bd77bdd67e0f0ba9cdfb758209df54
Didar Daniyarbekov <Didar.Daniyarbekov@gmail.com>
2022-09-27 16:58:51 +0600

    миграции моделей
    Модели зарегистрированы в admin
    провел миграции
    создан юзер Admin

--- market/webapp/admin.py
+++ market/webapp/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from webapp.models import Category, Product
 
 # Register your models here.
+class CategoryAdmin(admin.ModelAdmin):
+    fields = ('id','description','name','created_at')
+    list_display = ('id','description','name','created_at')
+    
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('id','description','name','price','created_at')
+    fields = ('id','description','name','price','created_at')
+
+
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Product, ProductAdmin)
