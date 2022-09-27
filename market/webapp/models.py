a3ba090dbc2d7207d2768e55972ec701fa4315dd
Didar Daniyarbekov <Didar.Daniyarbekov@gmail.com>
2022-09-27 16:43:01 +0600

    Созданы модели Category, Product

--- market/webapp/models.py
+++ market/webapp/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(verbose_name='Наименование', max_length=100, null=False, blank=False)
+    description = models.CharField(verbose_name='Описание', max_length=200, null=False, blank=False)
+    created_at = models.DateTimeField(verbose_name='Дата создания',auto_now_add=True)
+    changed_at = models.DateTimeField(verbose_name='Дата создания',auto_now=True)
+
+    def __str__(self) -> str:
+        return self.name
+
+class Product(models.Model):
+    name = models.CharField(verbose_name='Наименование', max_length=100, null=False, blank=False)
+    description = models.TextField(verbose_name='Описание',null=False, blank=False)
+    category = models.ForeignKey( verbose_name='Категория',to='webapp.Category',related_name='category',null=False, blank=False, on_delete=models.CASCADE)
+    price = models.DecimalField(verbose_name='Цена',null=False, blank=False, max_digits=10, decimal_places=3,)
+    created_at = models.DateTimeField(verbose_name='Дата добавления', default=timezone.now)
+    image = models.CharField(verbose_name='Изображения',max_length=100,null=False, blank=False)
+    changed_at = models.DateTimeField(verbose_name='Дата создания', default=timezone.now)
